03f489bf3bd42a44871cc8f6cda13577f91d5b1f
Gauri Khandke <khandkegauri@gmail.com>
2020-10-16 20:10:26 -0700

    Created workout schema

---
+++ models/exercise.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+  exercises: [
+    {
+      name : {
+        type : String,
+        trim : true,
+        required : "Please Enter Exercise name"
+      },
+      type : {
+        type: String,
+        trim : true,
+        required : "Please Enter Exercise type"
+      },
+      duration : {
+        type : Number,
+        required : "Please Enter Exercise duration"
+      },
+      weight: {
+        type : Number
+      },
+      sets: {
+        type : Number
+      },
+      reps: {
+        type : Number
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = Workout;
