b52c288b8e362c2498c7f7d5f8ca07f8429433b3
Gauri Khandke <khandkegauri@gmail.com>
2020-10-16 20:12:09 -0700

    Updated model path

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let db = require("../models/exercise");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
