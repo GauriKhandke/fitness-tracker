123a07c29efac1e79aac33eda0184bea280f5ea3
Gauri Khandke <khandkegauri@gmail.com>
2020-10-17 21:56:06 -0700

    Added api routes link to server js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker"
 });
 
 // routes
-// app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
