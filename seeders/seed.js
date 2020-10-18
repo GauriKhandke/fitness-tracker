f6f69141a4062c9772707d6f6e37702f306a35e7
Gauri Khandke <khandkegauri@gmail.com>
2020-10-17 22:49:33 -0700

    Updated db name in db connection

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models/exercise");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/fitnesstracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
