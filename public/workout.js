ee88b9e595e34abf70884f6c62d1abe6fddb032d
Gauri Khandke <khandkegauri@gmail.com>
2020-10-17 22:34:54 -0700

    Updated tally to show total duration

--- public/workout.js
+++ public/workout.js
@@ -25,8 +25,10 @@ function tallyExercises(exercises) {
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
       acc.totalSets = (acc.totalSets || 0) + curr.sets;
       acc.totalReps = (acc.totalReps || 0) + curr.reps;
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
     } else if (curr.type === "cardio") {
       acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
     }
     return acc;
   }, {});
